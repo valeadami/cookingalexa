b9e0b8bb1f93c24fbb1a52a48a0746f033598057
ADAMI VALENTINA <30869@ds.units.it>
2018-11-21 11:32:35 +0100

    la porta

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const options = {
     //modifica del 12/11/2018 : cambiato porta per supportare HTTPS
     
    hostname: '86.107.98.69', 
-  // port: 8080,
-   port: 8443,
+  port: 8080,
+   /*port: 8443,*/
    //rejectUnauthorized: false, // aggiunto qui 12/11/2018 
    path: '/AVA/rest/searchService/search_2?searchText=', 
    method: 'POST', 
